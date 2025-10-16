e19be8c5f224612fb6eb11384c139581a93bafb7
ajkfds <ajkfds2018@gmail.com>
2025-10-17 06:15:13 +0900

    fix parser

--- CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Tool/ParseHierarchy.cs
+++ CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Tool/ParseHierarchy.cs
@@ -19,21 +19,29 @@ namespace pluginVerilog.Tool
         private static CancellationTokenSource? _cts;
         public static async Task ParseAsync(CodeEditor2.Data.TextFile textFile)
         {
+            System.Diagnostics.Debug.Print("ParseAsync "+textFile.RelativePath);
             if (textFile.ParseValid && !textFile.ReparseRequested) return;
 
-//            if (_cts != null)
-//            {
-//                CodeEditor2.Controller.AppendLog("Cancelling previous parse...");
-//                textFile.ReparseRequested = true;
-//                _cts.Cancel();
+            System.Diagnostics.Debug.Print("Start " + textFile.RelativePath);
+            if (_cts != null)
+            {
+                System.Diagnostics.Debug.Print("Cancel " + textFile.RelativePath);
 
-//                try
-//                {
-//                    // wait completion of the previous task
-////                    if (_currentTask != null) await _currentTask;
-//                }
-//                catch (OperationCanceledException) { }
-//            }
+                CodeEditor2.Controller.AppendLog("Cancelling previous parse...");
+                textFile.ReparseRequested = true;
+                _cts.Cancel();
+
+                System.Diagnostics.Debug.Print("WaitCancel " + textFile.RelativePath);
+                try
+                {
+                    // wait completion of the previous task
+                    if (_currentTask != null) await _currentTask;
+                }
+                catch (OperationCanceledException) { }
+                System.Diagnostics.Debug.Print("CompleteCancel " + textFile.RelativePath);
+            }
+
+            System.Diagnostics.Debug.Print("Start2 " + textFile.RelativePath);
 
             _cts = new CancellationTokenSource();
             CancellationToken token = _cts.Token;
@@ -46,9 +54,16 @@ namespace pluginVerilog.Tool
             try
             {
                 await _currentTask;
+                System.Diagnostics.Debug.Print("Compelete " + textFile.RelativePath);
             }
             catch (OperationCanceledException)
             {
+                _currentTask = null;
+                System.Diagnostics.Debug.Print("Cancelled " + textFile.RelativePath);
+            }
+            finally
+            {
+                _currentTask = null;
             }
             return;
         }
@@ -85,7 +100,6 @@ namespace pluginVerilog.Tool
             }
             if (verilogFile == null) return;
 
-            CodeEditor2.Controller.AppendLog("parseHier : " + verilogFile.ID);
             token.ThrowIfCancellationRequested();
 
             if (!fileStack.Contains(textFile))
@@ -141,7 +155,7 @@ namespace pluginVerilog.Tool
             {
                 if(item is CodeEditor2.Data.TextFile)
                 {
-                    await ParseAsync((CodeEditor2.Data.TextFile)item);
+                    await parseText((CodeEditor2.Data.TextFile)item,token,fileStack);
                 }
             }
         }
