5392acf1e29b1dad097f0319c41b1cbf9c016adf
ajkfds <ajkfds2018@gmail.com>
2025-11-14 09:05:34 +0900

    add string methods

--- CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/DataObjects/Variables/String.cs
+++ CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/DataObjects/Variables/String.cs
@@ -22,59 +22,6 @@ namespace pluginVerilog.Verilog.DataObjects.Variables
             String val = new String() { Name = name };
             val.DataType = dataType;
 
-            //{ //function int len();
-            //    List<Port> ports = new List<Port>();
-            //    Variable returnVal = DataObjects.Variables.Int.Create("len", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Int, false));
-            //    BuiltInMethod builtInMethod = BuiltInMethod.Create("len", returnVal, ports);
-            //    val.NamedElements.Add(builtInMethod.Name, builtInMethod);
-            //}
-            //{ //function void putc(int i, byte c);
-            //    List<Port> ports = new List<Port>();
-            //    Port? port = Port.Create("i", null, Port.DirectionEnum.Input, DataObjects.Variables.Int.Create("i", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Int, false)));
-            //    if (port != null) ports.Add(port);
-            //    BuiltInMethod builtInMethod = BuiltInMethod.Create("putc", null, ports);
-            //    val.NamedElements.Add(builtInMethod.Name, builtInMethod);
-            //}
-            //{ //function byte getc(int i);
-            //}
-            ////function string toupper();
-            ////function string tolower();
-            ////function int compare(string s);
-            ////function int icompare(string s);
-            //{ //function string substr(int i, int j);
-            //    val.NamedElements.Add(substr.Name, substr);
-            //}
-            //{ //function integer atoi();
-            //    List<Port> ports = new List<Port>();
-            //    Variable returnVal = DataObjects.Variables.Integer.Create("atoi", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
-            //    BuiltInMethod builtInMethod = BuiltInMethod.Create("atoi", returnVal, ports);
-            //    val.NamedElements.Add(builtInMethod.Name, builtInMethod);
-            //}
-            //{ //function integer atohex();
-            //    List<Port> ports = new List<Port>();
-            //    Variable returnVal = DataObjects.Variables.Integer.Create("atohex", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
-            //    BuiltInMethod builtInMethod = BuiltInMethod.Create("atohex", returnVal, ports);
-            //    val.NamedElements.Add(builtInMethod.Name, builtInMethod);
-            //}
-            //{ //function integer atooct();
-            //    List<Port> ports = new List<Port>();
-            //    Variable returnVal = DataObjects.Variables.Integer.Create("atooct", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
-            //    BuiltInMethod builtInMethod = BuiltInMethod.Create("atooct", returnVal, ports);
-            //    val.NamedElements.Add(builtInMethod.Name, builtInMethod);
-            //}
-            ////function integer atobin();
-            //{
-            //    List<Port> ports = new List<Port>();
-            //    Variable returnVal = DataObjects.Variables.Integer.Create("atobin", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
-            //    BuiltInMethod builtInMethod = BuiltInMethod.Create("atobin", returnVal, ports);
-            //    val.NamedElements.Add(builtInMethod.Name, builtInMethod);
-            //}
-            ////function real atoreal();
-            ////function void itoa(integer i);
-            ////function void hextoa(integer i);
-            ////function void octtoa(integer i);
-            ////function void bintoa(integer i);
-            ////function void realtoa(real r);
             return val;
         }
 
@@ -87,6 +34,25 @@ namespace pluginVerilog.Verilog.DataObjects.Variables
                 if (namedElements != null) return namedElements;
 
                 namedElements = new NamedElements();
+                { //function int len();
+                    List<Port> ports = new List<Port>();
+                    Variable returnVal = DataObjects.Variables.Int.Create("len", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Int, false));
+                    BuiltInMethod builtInMethod = BuiltInMethod.Create("len", returnVal, ports);
+                    namedElements.Add(builtInMethod.Name, builtInMethod);
+                }
+                { //function void putc(int i, byte c);
+                    List<Port> ports = new List<Port>();
+                    Port? port = Port.Create("i", null, Port.DirectionEnum.Input, DataObjects.Variables.Int.Create("i", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Int, false)));
+                    if (port != null) ports.Add(port);
+                    BuiltInMethod builtInMethod = BuiltInMethod.Create("putc", null, ports);
+                    namedElements.Add(builtInMethod.Name, builtInMethod);
+                }
+                //{ //function byte getc(int i);
+                //}
+                ////function string toupper();
+                ////function string tolower();
+                ////function int compare(string s);
+                ////function int icompare(string s);
                 { //function string substr(int i, int j);
                     List<Port> ports = new List<Port>();
                     Port? port = Port.Create("i", null, Port.DirectionEnum.Input, DataObjects.Variables.Int.Create("i", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Int, false)));
@@ -97,6 +63,41 @@ namespace pluginVerilog.Verilog.DataObjects.Variables
                     BuiltInMethod builtInMethod = BuiltInMethod.Create("substr", returnVal, ports);
                     namedElements.Add(builtInMethod.Name, builtInMethod);
                 }
+                { //function integer atoi();
+                    List<Port> ports = new List<Port>();
+                    Variable returnVal = DataObjects.Variables.Integer.Create("atoi", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
+                    BuiltInMethod builtInMethod = BuiltInMethod.Create("atoi", returnVal, ports);
+                    namedElements.Add(builtInMethod.Name, builtInMethod);
+                }
+                { //function integer atohex();
+                    List<Port> ports = new List<Port>();
+                    Variable returnVal = DataObjects.Variables.Integer.Create("atohex", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
+                    BuiltInMethod builtInMethod = BuiltInMethod.Create("atohex", returnVal, ports);
+                    namedElements.Add(builtInMethod.Name, builtInMethod);
+                }
+                { //function integer atooct();
+                    List<Port> ports = new List<Port>();
+                    Variable returnVal = DataObjects.Variables.Integer.Create("atooct", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
+                    BuiltInMethod builtInMethod = BuiltInMethod.Create("atooct", returnVal, ports);
+                    namedElements.Add(builtInMethod.Name, builtInMethod);
+                }
+                { //function integer atobin();
+                    List<Port> ports = new List<Port>();
+                    Variable returnVal = DataObjects.Variables.Integer.Create("atobin", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Integer, false));
+                    BuiltInMethod builtInMethod = BuiltInMethod.Create("atobin", returnVal, ports);
+                    namedElements.Add(builtInMethod.Name, builtInMethod);
+                }
+                { //function real atoreal();
+                    List<Port> ports = new List<Port>();
+                    Variable returnVal = DataObjects.Variables.Real.Create("atoreal", DataTypes.IntegerAtomType.Create(DataTypes.DataTypeEnum.Real, false));
+                    BuiltInMethod builtInMethod = BuiltInMethod.Create("atoreal", returnVal, ports);
+                    namedElements.Add(builtInMethod.Name, builtInMethod);
+                }
+                ////function void itoa(integer i);
+                ////function void hextoa(integer i);
+                ////function void octtoa(integer i);
+                ////function void bintoa(integer i);
+                ////function void realtoa(real r);
 
                 return namedElements;
             }
