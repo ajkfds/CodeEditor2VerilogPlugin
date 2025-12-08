16c46647a76300f85dd8ba289759958d50ae9dc0
ajkfds <ajkfds2018@gmail.com>
2025-12-09 06:17:09 +0900

    fix autocomplete keyword

--- CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/ParsedDocument.cs
+++ CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/ParsedDocument.cs
@@ -4,6 +4,7 @@ using CodeEditor2.CodeEditor.Parser;
 using CodeEditor2.CodeEditor.PopupHint;
 using ExCSS;
 using pluginVerilog.CodeEditor;
+using pluginVerilog.FileTypes;
 using pluginVerilog.NavigatePanel;
 using pluginVerilog.Verilog.BuildingBlocks;
 using pluginVerilog.Verilog.DataObjects;
@@ -16,6 +17,7 @@ using System.Linq;
 using System.Net.Http.Headers;
 using System.Reflection.Metadata;
 using System.Runtime.InteropServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.Json.Serialization;
 using System.Threading.Tasks;
@@ -32,6 +34,8 @@ namespace pluginVerilog.Verilog
             if (document == null) throw new Exception();
             codeDocument = document;
 
+            if (file is SystemVerilogFile || file is SystemVerilogHeaderFile) SystemVerilog = true;
+
             fileRef = new WeakReference<Data.IVerilogRelatedFile>(file);
             if(indexReference == null)
             {
@@ -46,6 +50,8 @@ namespace pluginVerilog.Verilog
         [Newtonsoft.Json.JsonConstructor]
         private ParsedDocument(Data.IVerilogRelatedFile file, string key, IndexReference? indexReference) : base((CodeEditor2.Data.TextFile)file, key, long.MaxValue, DocumentParser.ParseModeEnum.LoadParse)
         {
+            if (file is SystemVerilogFile || file is SystemVerilogHeaderFile) SystemVerilog = true;
+
             System.Diagnostics.Debug.Print("## parsed " + key);
             if (file == null)
             {
@@ -121,6 +127,7 @@ namespace pluginVerilog.Verilog
                 return ret;
             }
         }
+
 
         [Newtonsoft.Json.JsonIgnore]
         public IndexReference IndexReference;
@@ -566,7 +573,7 @@ namespace pluginVerilog.Verilog
                 // special autocomplete tool
                 AppendSpecialAutoCompleteItems(items, candidateWord);
                 // keywords
-                AppendKeywordAutoCompleteItems(items, candidateWord);
+                AppendKeywordAutoCompleteItems(items, candidateWord,SystemVerilog);
             }
 
             // on Root and sont have ant Building Blocks
@@ -723,78 +730,146 @@ namespace pluginVerilog.Verilog
             if (nameSpace.Parent is NameSpace) appendAutoCompleteINamedElements(items, nameSpace.Parent, candidate);
         }
 
-        public delegate void AppendKeywordAutoCompleteItemsDelegate(List<AutocompleteItem> items, string cantidate);
+        public delegate void AppendKeywordAutoCompleteItemsDelegate(List<AutocompleteItem> items, string cantidate,bool systemVerilog);
         public static AppendKeywordAutoCompleteItemsDelegate AppendKeywordAutoCompleteItems = appendKeywordAutoCompleteItems;
 
-        private static void appendKeywordAutoCompleteItems(List<AutocompleteItem> items,string cantidate)
+        private static void appendKeywordAutoCompleteItems(List<AutocompleteItem> items,string cantidate, bool systemVerilog)
         {
-            List<string> keywords = new List<string>
-                    {
-                        // Verilog
-                        "always",       "and",          "assign",       "automatic",
-                        /*"begin",*/    "case",         "casex",        "casez",
-                        "deassign",     "default",      "defparam",     "design",
-                        "disable",      "edge",         "else",         "end",
-                        "endcase",      "endfunction",  "endgenerate",  "endmodule",
-                        "endprimitive", "endspecify",   "endtask",      "for",
-                        "force",        "forever",      "fork",         /*"function",*/
-                        /*"generate",*/ "genvar",       "if",           "incdir",
-                        "include",      "initial",      "inout",        "input",
-                        "integer",      /*"interface",*/"join",         "localparam",
-                        /*"module",*/   "nand",         "negedge",      "nor",
-                        "not",          "or",           "output",       "parameter",
-                        "posedge",      "pulldown",     "pullup",       "real",
-                        "realtime",     "reg",          "release",      "repeat",
-                        "signed",       /*"task",*/     "time",         "tri0",
-                        "tri1",         "trireg",       "unsigned",     "vectored",
-                        "wait",         "wand",         "weak0",        "weak1",
-                        "while",        "wire",         "wor",
-                        // SystemVerilog
-                        "accept_on",    "alias",        "always_comb",  "always_ff",
-                        "always_latch", "assert",       "assume",       "before",
-                        "bind",         "bins",         "binsof",       "bit",
-                        "break",        "byte",         "chandle",      "checker",
-                        "class",        "clocking",     "const",        "constraint",
-                        "context",      "continue",     "cover",        "covergroup",
-                        "coverpoint",   "cross",        "dist",         "do",
-                        "endchecker",   "endclass",     "endclocking",  "endgroup",
-                        "endinterface", "endpackage",   "endprogram",   "endproperty",
-                        "endsequence",  "enum",         "eventually",   "expect",
-                        "export",       "extends",      "extern",       "final",
-                        "first_match",  "foreach",      "forkjoin",     "global",
-                        "iff",          "ignore_bins",  "illegal_bins", "implements",
-                        "implies",      "import",       "inside",       "int",
-                        "interconnect", "interface",    "intersect",    "join_any",
-                        "join_none",    "let",          "local",        "logic",
-                        "longint",      "matches",      "modport",      "nettype",
-                        "new",          "nexttime",     "null",         "package",
-                        "packed",       "priority",     "program",      "property",
-                        "protected",    "pure",         "rand",         "randc",
-                        "randcase",     "randsequence", "ref",          "reject_on",
-                        "restrict",     "return",       "s_always",     "s_eventually",
-                        "s_nexttime",   "s_until",      "s_until_with", "sequence",
-                        "shortint",     "shortreal",    "soft",         "solve",
-                        "static",       "string",       "strong",       "struct",
-                        "super",        "sync_accept_on",   "sync_reject_on",   "tagged",
-                        "this",         "throughout",   "timeprecision",    "timeunit",
-                        "type",         "typedef",      "union",        "unique",
-                        "unique0",      "until",        "until_with",   "untyped",
-                        "uwire",        "var",          "virtual",      "void",
-                        "wait_order",   "weak",         "wildcard",     "with",
-                        "within",
-                    };
+            
+            List<(string, int)> keywords = new List<(string, int)>
+                {
+                // Verilog
+                ("always",1),
+                ("and",1),
+                ("assign",1),
+                ("automatic",2),
+                /*"begin",*/    
+                ("case",1),
+                ("casex",1),
+                ("casez",1),
 
-            foreach (string keyword in keywords)
+                ("deassign",1),
+                ("default",1),
+                ("defparam",1),
+                ("design",1),
+                ("disable",1),
+                ("edge",1),
+                ("else",1),
+                ("end",1),
+                ("endcase",1),
+                ("endfunction",1),
+                ("endgenerate",1),
+                ("endmodule",1),
+                ("endprimitive",1),
+                ("endspecify",1),
+                ("endtask",1),
+                ("for",1),
+                ("force",1),
+                ("forever",1),
+                ("fork",1),
+                /*"function",1),*/
+                /*"generate",1),*/
+                ("genvar",1),
+                ("if",1),
+                ("incdir",2),
+                ("include",1),
+                ("initial",1),
+                ("inout",1),
+                ("input",1),
+                ("integer",1),
+                /*"interface",1),*/
+                ("join",1),
+                ("localparam",1),
+                /*"module",1),*/  
+                ("nand",1),
+                ("negedge",1),
+                ("nor",1),
+                ("not",1),
+                ("or",1),
+                ("output",1),
+                ("parameter",1),
+                ("posedge",1),
+                ("pulldown",1),    ("pullup",1),       ("real",1),
+               ("realtime",1),    ("reg",1),         ("release",1),      ("repeat",1),
+               ("signed",1),       /*"task",1),*/    ("time",1),         ("tri0",1),
+               ("tri1",1),        ("trireg",1),      ("unsigned",1),    ("vectored",1),
+               ("wait",1),        ("wand",1),        ("weak0",1),       ("weak1",1),
+               ("while",1),       ("wire",1),        ("wor",1)};
+            List<(string, int)> systemVerilogKeywords = new List<(string, int)> {
+               // SystemVerilog
+               ("accept_on",1),
+                ("alias",1),
+                ("always_comb",1),
+                ("always_ff",1),
+               ("always_latch",1),
+                ("assert",1),
+                ("assume",1),
+                ("before",1),
+               ("bind",1),
+                ("bins",1),        ("binsof",1),      ("bit",1),
+               ("break",1),       ("byte",1),        ("chandle",1),     ("checker",1),
+               ("class",1),       ("clocking",1),    ("const",1),       ("constraint",1),
+               ("context",1),     ("continue",1),    ("cover",1),       ("covergroup",1),
+               ("coverpoint",1),  ("cross",1),       ("dist",1),        ("do",1),
+               ("endchecker",1),  ("endclass",1),    ("endclocking",1), ("endgroup",1),
+               ("endinterface",1),("endpackage",1),  ("endprogram",1),  ("endproperty",1),
+               ("endsequence",1), ("enum",1),        ("eventually",1),  ("expect",1),
+               ("export",1),      ("extends",1),     ("extern",1),      ("final",1),
+               ("first_match",1), ("foreach",1),     ("forkjoin",1),    ("global",1),
+               ("iff",1),         ("ignore_bins",1), ("illegal_bins",1),("implements",1),
+               ("implies",1),     ("import",1),      ("inside",1),      ("int",1),
+               ("interconnect",1),("interface",1),   ("intersect",1),   ("join_any",1),
+               ("join_none",1),   ("let",1),         ("local",1),       ("logic",1),
+               ("longint",1),     ("matches",1),     ("modport",1),     ("nettype",1),
+               ("new",1),         ("nexttime",1),    ("null",1),        ("package",1),
+               ("packed",1),      ("priority",1),    ("program",1),     ("property",1),
+               ("protected",1),   ("pure",1),        ("rand",1),        ("randc",1),
+               ("randcase",1),    ("randsequence",1),("ref",1),         ("reject_on",1),
+               ("restrict",1),    ("return",1),      ("s_always",1),    ("s_eventually",1),
+               ("s_nexttime",1),  ("s_until",1),     ("s_until_with",1),("sequence",1),
+               ("shortint",1),    ("shortreal",1),   ("soft",1),        ("solve",1),
+               ("static",1),      ("string",1),      ("strong",1),      ("struct",1),
+               ("super",1),       ("sync_accept_on",1),  ("sync_reject_on",1),  ("tagged",1),
+               ("this",1),        ("throughout",1),  ("timeprecision",1),   ("timeunit",1),
+               ("type",1),        ("typedef",1),     ("union",1),       ("unique",1),
+               ("unique0",1),     ("until",1),       ("until_with",1),  ("untyped",1),
+               ("uwire",1),       ("var",1),         ("virtual",1),     ("void",1),
+               ("wait_order",1),  ("weak",1),        ("wildcard",1),    ("with",1),
+               ("within",1)
+            };
+
+
+            
+            foreach ((string,int) keyword in keywords)
             {
-                if (!keyword.StartsWith(cantidate)) continue;
+                if (!keyword.Item1.StartsWith(cantidate)) continue;
+                if (cantidate.Length < keyword.Item2) continue;
                 AutocompleteItem item = new CodeEditor2.CodeEditor.CodeComplete.AutocompleteItem(
-                    keyword,
+                    keyword.Item1,
                     CodeDrawStyle.ColorIndex(CodeDrawStyle.ColorType.Keyword),
                     Global.CodeDrawStyle.Color(CodeDrawStyle.ColorType.Keyword),
                     "CodeEditor2/Assets/Icons/bookmark.svg"
                     );
                 items.Add(item);
             }
+
+            if (systemVerilog)
+            {
+                foreach ((string, int) keyword in systemVerilogKeywords)
+                {
+                    if (!keyword.Item1.StartsWith(cantidate)) continue;
+                    if (cantidate.Length < keyword.Item2) continue;
+                    AutocompleteItem item = new CodeEditor2.CodeEditor.CodeComplete.AutocompleteItem(
+                        keyword.Item1,
+                        CodeDrawStyle.ColorIndex(CodeDrawStyle.ColorType.Keyword),
+                        Global.CodeDrawStyle.Color(CodeDrawStyle.ColorType.Keyword),
+                        "CodeEditor2/Assets/Icons/bookmark.svg"
+                        );
+                    items.Add(item);
+                }
+
+            }
+
         }
         public delegate void AppendSpecialAutoCompleteItemsDelegate(List<AutocompleteItem> items, string cantidate);
         [Newtonsoft.Json.JsonIgnore]
