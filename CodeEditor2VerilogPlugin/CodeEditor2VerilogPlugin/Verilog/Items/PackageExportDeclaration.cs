69ba3ac8934361b7334f0081dcba73aecff1d507
ajkfds <ajkfds2018@gmail.com>
2026-05-10 09:00:06 +0900

    add package

--- CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/Items/PackageExportDeclaration.cs
+++ CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/Items/PackageExportDeclaration.cs
@@ -8,43 +8,162 @@ namespace pluginVerilog.Verilog.Items
             | export package_import_item { , package_import_item } ;
         package_import_item ::=
               package_identifier :: identifier
-            | package_identifier :: *         
-         
+            | package_identifier :: *
+
+        package_import_declaration ::=
+              import package_identifier :: item_identifier { , item_identifier }
+            | import package_identifier :: *
          */
-        public void Parse(WordScanner word, NameSpace nameSpace)
+
+        public static void ParseExportDeclaration(WordScanner word, NameSpace nameSpace)
         {
-            if (word.Text != "export" && System.Diagnostics.Debugger.IsAttached) System.Diagnostics.Debugger.Break();
+            if (word.Text != "export") System.Diagnostics.Debugger.Break();
             word.Color(CodeDrawStyle.ColorType.Keyword);
             word.MoveNext();
 
             if (word.Text == "*")
             {
+                // export *::* ;
                 word.Color(CodeDrawStyle.ColorType.Identifier);
                 word.MoveNext();
-                if (word.Text != "::" && System.Diagnostics.Debugger.IsAttached) System.Diagnostics.Debugger.Break();
+                if (word.Text != "::") System.Diagnostics.Debugger.Break();
                 word.Color(CodeDrawStyle.ColorType.Identifier);
                 word.MoveNext();
-                if (word.Text != "*" && System.Diagnostics.Debugger.IsAttached) System.Diagnostics.Debugger.Break();
+                if (word.Text != "*") System.Diagnostics.Debugger.Break();
                 word.Color(CodeDrawStyle.ColorType.Identifier);
                 word.MoveNext();
             }
             else
             {
-                //while (true)
-                //{
-                //    if (!PackageImportItem.Parse(word, nameSpace))
-                //    {
-                //        if (System.Diagnostics.Debugger.IsAttached) System.Diagnostics.Debugger.Break();
-                //        return false;
-                //    }
-                //    if (word.Text != ",")
-                //    {
-                //        break;
-                //    }
-                //    word.Color(CodeDrawStyle.ColorType.Normal);
-                //    word.MoveNext();
-                //}
+                // export package_import_item { , package_import_item } ;
+                ParsePackageImportItems(word, nameSpace);
             }
+        }
+
+        public static void ParseImportDeclaration(WordScanner word, NameSpace nameSpace)
+        {
+            /*
+            package_import_declaration ::=
+                  import package_identifier :: item_identifier { , item_identifier }
+                | import package_identifier :: *
+             */
+            if (word.Text != "import") System.Diagnostics.Debugger.Break();
+            word.Color(CodeDrawStyle.ColorType.Keyword);
+            word.MoveNext();
+
+            // Parse single item or wildcard import
+            while (true)
+            {
+                // Parse package_identifier
+                if (!General.IsSimpleIdentifier(word.Text))
+                {
+                    word.AddError("package identifier expected");
+                    word.SkipToKeyword(";");
+                    break;
+                }
+                word.Color(CodeDrawStyle.ColorType.Identifier);
+                word.MoveNext();
+
+                if (word.Text != "::")
+                {
+                    word.AddError(":: expected");
+                    word.SkipToKeyword(";");
+                    break;
+                }
+                word.Color(CodeDrawStyle.ColorType.Identifier);
+                word.MoveNext();
+
+                if (word.Text == "*")
+                {
+                    // import package_identifier :: * ;
+                    // Wildcard import - import all items from the package
+                    word.Color(CodeDrawStyle.ColorType.Identifier);
+                    word.MoveNext();
+                }
+                else
+                {
+                    // import package_identifier :: item_identifier
+                    if (!General.IsSimpleIdentifier(word.Text))
+                    {
+                        word.AddError("identifier expected");
+                        word.SkipToKeyword(";");
+                        break;
+                    }
+                    word.Color(CodeDrawStyle.ColorType.Identifier);
+                    word.MoveNext();
+                }
+
+                if (word.Text != ",")
+                {
+                    break;
+                }
+                word.Color(CodeDrawStyle.ColorType.Normal);
+                word.MoveNext();
+            }
+        }
+
+        private static void ParsePackageImportItems(WordScanner word, NameSpace nameSpace)
+        {
+            /*
+            package_import_item ::=
+                  package_identifier :: identifier
+                | package_identifier :: *
+             */
+            while (true)
+            {
+                // Parse package_identifier
+                if (!General.IsSimpleIdentifier(word.Text))
+                {
+                    word.AddError("package identifier expected");
+                    word.SkipToKeyword(";");
+                    break;
+                }
+                string packageName = word.Text;
+                word.Color(CodeDrawStyle.ColorType.Identifier);
+                word.MoveNext();
+
+                if (word.Text != "::")
+                {
+                    word.AddError(":: expected");
+                    word.SkipToKeyword(";");
+                    break;
+                }
+                word.Color(CodeDrawStyle.ColorType.Identifier);
+                word.MoveNext();
+
+                if (word.Text == "*")
+                {
+                    // package_identifier :: *
+                    word.Color(CodeDrawStyle.ColorType.Identifier);
+                    word.MoveNext();
+                }
+                else
+                {
+                    // package_identifier :: identifier
+                    if (!General.IsSimpleIdentifier(word.Text))
+                    {
+                        word.AddError("identifier expected");
+                        word.SkipToKeyword(";");
+                        break;
+                    }
+                    word.Color(CodeDrawStyle.ColorType.Identifier);
+                    word.MoveNext();
+                }
+
+                if (word.Text != ",")
+                {
+                    break;
+                }
+                word.Color(CodeDrawStyle.ColorType.Normal);
+                word.MoveNext();
+            }
+        }
+
+        // Keep old method for backward compatibility
+        [System.Obsolete("Use ParseExportDeclaration instead")]
+        public void Parse(WordScanner word, NameSpace nameSpace)
+        {
+            ParseExportDeclaration(word, nameSpace);
         }
     }
 }
--- CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/Items/PackageItem.cs
+++ CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/Items/PackageItem.cs
@@ -15,12 +15,27 @@ namespace pluginVerilog.Verilog.Items
 
     public class PackageItem
     {
+        /*
+        package_item ::=
+              package_or_generate_item_declaration 
+            | anonymous_program 
+            | package_export_declaration 
+            | timeunits_declaration
+        */
         public static async System.Threading.Tasks.Task<bool> Parse(WordScanner word, NameSpace nameSpace)
         {
             switch (word.Text)
             {
                 // anonymous_program
+                case "program":
+                    return await BuildingBlocks.Program.Parse(word, null, nameSpace.BuildingBlock, word.RootParsedDocument.File, word.Prototype) != null;
                 // package_export_declaration
+                case "export":
+                    PackageExportDeclaration.ParseExportDeclaration(word, nameSpace);
+                    return true;
+                case "import":
+                    PackageExportDeclaration.ParseImportDeclaration(word, nameSpace);
+                    return true;
                 // timeunits_declaration
                 default:
                     return await PackageOrGenerateItemDeclaration.Parse(word, nameSpace);
