c30c0bcb7372bab110eab72a29217c56cdf7df02
ajkfds <ajkfds2018@gmail.com>
2026-05-11 06:09:12 +0900

    disable fork

--- CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/Statements/ParallelBlock.cs
+++ CodeEditor2VerilogPlugin/CodeEditor2VerilogPlugin/Verilog/Statements/ParallelBlock.cs
@@ -6,6 +6,16 @@ using System.Threading.Tasks;
 
 namespace pluginVerilog.Verilog.Statements
 {
+    /// <summary>
+    /// Join variant types for parallel blocks
+    /// </summary>
+    public enum JoinVariant
+    {
+        Join,       // join - wait for all processes to complete
+        JoinAny,    // join_any - wait for at least one process to complete
+        JoinNone    // join_none - don't wait, continue immediately
+    }
+
     public class ParallelBlock : IStatement
     {
         protected ParallelBlock() { }
@@ -31,6 +41,11 @@ namespace pluginVerilog.Verilog.Statements
         }
 
         public List<IStatement> Statements = new List<IStatement>();
+
+        /// <summary>
+        /// Join variant type
+        /// </summary>
+        public JoinVariant JoinType { get; set; } = JoinVariant.Join;
 
         /*
         A.6.3 Parallel and sequential blocks
@@ -59,7 +74,6 @@ namespace pluginVerilog.Verilog.Statements
         private static async Task<ParallelBlock> parseParallelBlock(WordScanner word, NameSpace nameSpace, IndexReference beginIndex)
         {
             ParallelBlock sequentialBlock = new ParallelBlock();
-            List<string> join_families = new List<string> { "join", "join_any", "join_none" };
 
             while (!word.Eof && !join_families.Contains(word.Text))
             {
@@ -72,6 +86,21 @@ namespace pluginVerilog.Verilog.Statements
                 word.AddError("illegal sequential block");
                 return null;
             }
+
+            // Store the join variant type
+            switch (word.Text)
+            {
+                case "join":
+                    sequentialBlock.JoinType = JoinVariant.Join;
+                    break;
+                case "join_any":
+                    sequentialBlock.JoinType = JoinVariant.JoinAny;
+                    break;
+                case "join_none":
+                    sequentialBlock.JoinType = JoinVariant.JoinNone;
+                    break;
+            }
+
             word.Color(CodeDrawStyle.ColorType.Keyword);
             word.MoveNext(); // end
 
@@ -79,6 +108,8 @@ namespace pluginVerilog.Verilog.Statements
         }
 
         private static List<string> endKeyword = new List<string> { "endmodule", "endtask", "endtask", "endinterface", "endfunction" };
+        private static List<string> join_families = new List<string> { "join", "join_any", "join_none" };
+
         private static async Task<IStatement> parseNamedParallelBlock(WordScanner word, NameSpace nameSpace, IndexReference beginIndex)
         {
             NamedParallelBlock namedBlock;
@@ -135,7 +166,7 @@ namespace pluginVerilog.Verilog.Statements
             }
             word.MoveNext();
 
-            while (!word.Eof && word.Text != "join")
+            while (!word.Eof && !join_families.Contains(word.Text))
             {
                 IStatement? statement = null;
                 statement = await Verilog.Statements.Statements.ParseCreateStatement(word, namedBlock);
@@ -143,15 +174,30 @@ namespace pluginVerilog.Verilog.Statements
                 namedBlock.Statements.Add(statement);
             }
 
-            if (word.Text != "join")
+            if (!join_families.Contains(word.Text))
             {
                 word.AddError("illegal sequential block");
                 namedBlock.LastIndexReference = word.CreateIndexReference();
                 return namedBlock;
             }
+
+            // Store the join variant type
+            switch (word.Text)
+            {
+                case "join":
+                    namedBlock.JoinType = JoinVariant.Join;
+                    break;
+                case "join_any":
+                    namedBlock.JoinType = JoinVariant.JoinAny;
+                    break;
+                case "join_none":
+                    namedBlock.JoinType = JoinVariant.JoinNone;
+                    break;
+            }
+
             word.Color(CodeDrawStyle.ColorType.Keyword);
             namedBlock.LastIndexReference = word.CreateIndexReference();
-            word.MoveNext(); // join
+            word.MoveNext(); // join/join_any/join_none
 
             if (word.Text == ":")
             {
@@ -195,6 +241,67 @@ namespace pluginVerilog.Verilog.Statements
         }
 
         public List<IStatement> Statements = new List<IStatement>();
+
+        /// <summary>
+        /// Join variant type
+        /// </summary>
+        public JoinVariant JoinType { get; set; } = JoinVariant.Join;
     }
 
+    /// <summary>
+    /// Disable Fork Statement
+    /// disable_statement ::= disable hierarchical_task_identifier ;
+    ///                      | disable fork ;
+    /// 
+    /// The disable fork statement terminates all active processes that were spawned by 
+    /// fork...join_none blocks in the current thread's context.
+    /// </summary>
+    public class DisableForkStatement : IStatement
+    {
+        protected DisableForkStatement() { }
+
+        public string Name { get; protected set; } = "disable fork";
+        public CodeDrawStyle.ColorType ColorType => CodeDrawStyle.ColorType.Identifier;
+        public NamedElements NamedElements => new NamedElements();
+
+        public static DisableForkStatement ParseCreate(WordScanner word, NameSpace nameSpace, string? statement_label)
+        {
+            if (word.Text != "disable") return null;
+            if (word.NextText != "fork") return null;
+
+            DisableForkStatement statement = new DisableForkStatement();
+            word.Color(CodeDrawStyle.ColorType.Keyword);
+            word.MoveNext(); // disable
+
+            word.Color(CodeDrawStyle.ColorType.Keyword);
+            word.MoveNext(); // fork
+
+            // Semicolon
+            if (word.Text == ";")
+            {
+                word.Color(CodeDrawStyle.ColorType.Keyword);
+                word.MoveNext();
+            }
+            else
+            {
+                word.AddError("; expected");
+            }
+
+            return statement;
+        }
+
+        public void DisposeSubReference()
+        {
+        }
+
+        public AutocompleteItem CreateAutoCompleteItem()
+        {
+            return new AutocompleteItem(
+                Name,
+                CodeDrawStyle.ColorIndex(ColorType),
+                Global.CodeDrawStyle.Color(ColorType),
+                "CodeEditor2/Assets/Icons/tag.svg"
+            );
+        }
+    }
 }
